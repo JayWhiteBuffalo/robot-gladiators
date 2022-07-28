30fbf54fe4614d8af2872a74703136e1f11c1393
White Buffalo <apothocare@gmail.com>
2022-07-28 16:55:06 -0500

    added var pickedenemyname to for loop to reset health of bot after defeat and move to next item in array

--- assets/game.js
+++ assets/game.js
@@ -50,11 +50,11 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-
-var fight = function(enemyName) {
   //Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
-
+var fight = function(enemyName) {
+  //repeat and execute as long as the enemy-robot is alive
+  while(enemyHealth > 0) {
   //If Player Chooses to Fight
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
   if (promptFight === "FIGHT" || promptFight === "fight" ){
@@ -102,10 +102,14 @@ if (playerHealth <=0) {
     window.alert("You need to choose a valid option. Try again!");
   }
   //fight function statements
+  }
 };
 
-for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+for (var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  // call fight function with enemy-robot
+  fight(pickedEnemyName);
 }
 
 
